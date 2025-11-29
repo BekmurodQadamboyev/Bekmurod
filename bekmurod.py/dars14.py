08b299752c7911145f05583c8e4f9f42ffde1281
BekmurodQadamboyev <bekmurodqadamboyev09@gmail.com>
2025-11-29 20:54:47 +0500

    Update dars14.py

--- bekmurod.py/dars14.py
+++ bekmurod.py/dars14.py
@@ -2,3 +2,21 @@ son = int(input())
 son2 = int(input())
 son3 = son + son2
 print(son3)
+#1-masala
+k = int(input("k = "))
+n = int(input("n = "))
+for i in range(n):
+    print(n)
+#2-masala
+a = int(input("a = "))
+b = int(input("b = "))
+count = 0
+for i ni range(a, b+1)
+    print(i)
+    count = count + 1
+print("Sikl", count, "marta takrollanadi")
+#3-masala
+
+
+
+
