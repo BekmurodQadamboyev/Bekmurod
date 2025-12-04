6b2ccbc7c964dd082e4684eb3990878909782644
BekmurodQadamboyev <bekmurodqadamboyev09@gmail.com>
2025-12-04 17:57:27 +0500

    Update dars14.py

--- bekmurod.py/dars14.py
+++ bekmurod.py/dars14.py
@@ -73,64 +73,101 @@
 
 #13-masala
 n = int(input("n = "))
-n > 0
-S = 1
-for i in range(1, n + 1):
-     S *= i
-print("Ko‘paytma =", S)
+# n > 0
+# S = 1
+# for i in range(1, n + 1):
+#      S *= i
+# print("Ko‘paytma =", S)
 
 
-#2-m
-a = int(input("a = "))
-b = int(input("b = "))
+# #2-m
+# a = int(input("a = "))
+# b = int(input("b = "))
 
-for i in range(a, b + 1):
-    print(i)
-#3-m
-a = int(input("a = "))
-b = int(input("b = "))
+# for i in range(a, b + 1):
+#     print(i)
+# #3-m
+# a = int(input("a = "))
+# b = int(input("b = "))
 
-for i in range(b, a - 1, -1):
-    print(i)
+# for i in range(b, a - 1, -1):
+#     print(i)
 
-#4-m
-p = float(input("1 kg narxi = "))
+# #4-m
+# p = float(input("1 kg narxi = "))
 
-for i in range(1, 10 + 1):
-    print(f"{i} kg = {p * i}")
+# for i in range(1, 10 + 1):
+#     print(f"{i} kg = {p * i}")
 
-#5-m
-p = float(input("1 kg narxi = "))
+# #5-m
+# p = float(input("1 kg narxi = "))
 
-for i in range(1, 10):
-    kg = i / 10
-    print(f"{kg} kg = {p * kg}")
-#6-m
-p = float(input("1 kg narxi = "))
+# for i in range(1, 10):
+#     kg = i / 10
+#     print(f"{kg} kg = {p * kg}")
+# #6-m
+# p = float(input("1 kg narxi = "))
 
-kg = 1.2
-while kg <= 2.0:
-    print(f"{kg} kg = {p * kg}")
-    kg += 0.2
+# kg = 1.2
+# while kg <= 2.0:
+#     print(f"{kg} kg = {p * kg}")
+#     kg += 0.2
 
-#7-m
+# #7-m
 
-a = int(input("a = "))
-b = int(input("b = "))
+# a = int(input("a = "))
+# b = int(input("b = "))
 
-s = 0
-for i in range(a, b + 1):
-    s += i
+# s = 0
+# for i in range(a, b + 1):
+#     s += i
 
-print("Yig'indi =", s)
+# print("Yig'indi =", s)
 
 
-#8-m
-a = int(input("a = "))
-b = int(input("b = "))
+# #8-m
+# a = int(input("a = "))
+# b = int(input("b = "))
 
-p = 1
-for i in range(a, b + 1):
-    p *= i
+# p = 1
+# for i in range(a, b + 1):
+#     p *= i
 
-print("Ko'paytma =", p)
+# print("Ko'paytma =", p)
+
+
+# ######################################################################################
+# #While-1
+
+# A = int(input("A sonni kiriting"))
+# B = int(input("B sonni kiriting"))
+
+# while A >= B :
+#     A -= B
+# print(A)
+
+#W-2
+
+#A = int(input("A sonni kiriting"))
+#  B = int(input("B sonni kiriting"))
+
+#  while A >= B :
+#      A -= B
+#      count = count + 1
+#  print(count)
+
+while True :
+    yosh = int(input("Yoshni kiriting"))
+    if yosh <= 7 :
+       narx = 2000
+    elif yosh <= 18 :
+       narx = 3000
+    elif yosh <= 65 :
+       narx = 10_000
+    else :
+       narx = 0
+print(f"Sizning chipta narxingiz {narx} So'm")
+javob = input("Yoshni kiriting yoki dasturni tugarish uchun 'Exit' yoki 'Cuit' ")
+if (javob.lower() == 'quit' or javob.lower() == 'exit') :
+   break
+
